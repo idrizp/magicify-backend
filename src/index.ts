0082eca61581f11b6895cac2ca1a86ca6ed06953
idrizp <idrizpelaj@hotmail.com>
2021-05-09 13:48:12 +0200

    It's done!

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import * as express from "express";
 import * as helmet from "helmet";
 import * as cors from "cors";
 import { MongoClient } from "mongodb";
 import * as path from "path";
-import {v4 as generateUUID } from "uuid";
+import { v4 as generateUUID } from "uuid";
 import multer = require("multer");
+import * as fs from "fs";
+import * as FileType from "file-type";
 
 interface Item {
 	name: string;
@@ -12,6 +17,14 @@ interface Item {
 	modelPath: string;
 	iconPath: string;
 }
+
+const extensions = {
+	"image/png": ".png",
+	"image/svg+xml": ".svg",
+	"model/gltf-binary": ".gltb",
+	"model/gltf+json": ".gltf",
+	"application/octet-stream": ""
+};
 
 (async () => {
 	const client = new MongoClient(process.env.MONGO_URI);
@@ -26,6 +39,8 @@ interface Item {
 				case "image/svg+xml":
 					callback(null, path.join(__dirname, "../icons"));
 					return;
+				case "model/gltf+json":
+				case "application/octet-stream":
 				case "model/gltf-binary":
 					callback(null, path.join(__dirname, "../models"));
 					return;
@@ -34,18 +49,31 @@ interface Item {
 					return;
 			}
 		},
+		filename: (req, file, callback) => {
+			callback(null, generateUUID() + extensions[file.mimetype]);
+		}
 	});
 
-	const allowedTypes = ["model/gltf-binary", "image/png", "image/svg+xml"];
+	const allowedTypes = ["model/gltf-binary", "model/gltf+json", "image/png", "image/svg+xml"];
 	const upload = multer({
 		storage,
-		fileFilter: (req, file, callback) => {
-			if (allowedTypes.indexOf(file.mimetype) === -1 || file.size > (1024 * 1000 * 200)) {
+		fileFilter: async (req, file, callback) => {
+			console.log("FILTERING NOW");
+			let type: string = file.mimetype;
+			console.log("TYPE IS " + type);
+			if (type === "application/octet-stream") {
+				// Seriously not secure but yea, fuck multer ig
+				if (file.originalname.endsWith(".gltf") || file.originalname.endsWith(".glb")) {
+					callback(null, true);
+					return;
+				}
+			}
+			if (allowedTypes.indexOf(type) === -1) {
 				callback(null, false);
 				return;
 			}
 			callback(null, true);
-		}
+		},
 	});
 
 
@@ -57,14 +85,9 @@ interface Item {
 
 	app.use("/models", express.static(path.join(__dirname, "../models")));
 	app.use("/icons", express.static(path.join(__dirname, "../icons")));
+
 	app.get("/", async (req, res) => {
-		let page: number = Number.parseInt(req.query.page as string, 10) || 1;
-		if (page === NaN) page = 1;
-
-		const items = collection.find()
-			.skip((page - 1) * 10)
-			.limit(page);
-
+		const items = collection.find();
 		const array = (await items.toArray()).map(item => {
 			delete item._id;
 			return item;
@@ -73,19 +96,21 @@ interface Item {
 		res.status(200).json(array);
 	});
 
-	app.post("/", async (req, res) => {
+	app.post("/", upload.fields([{ name: "icon", maxCount: 1 }, { name: "model", maxCount: 1 }]), async (req, res) => {
 		const name = req.body.name;
 		const found = await collection.findOne({ name: name });
+
+		console.log(req.files);
+
+		const iconFile: Express.Multer.File | undefined = req.files["icon"][0];
+		const modelFile: Express.Multer.File | undefined = req.files["model"][0];
+
 		if (found) {
 			res.status(409).json({ error: "A model by that name already exists." });
+			fs.unlinkSync(iconFile.path);
+			fs.unlinkSync(modelFile.path);
 			return;
 		};
-		upload.fields([{name: "icon", maxCount: 1}, {name: "model", maxCount: 1}])(req, res, (error: any) => {
-			res.status(500).json({ error: "Something went wrong when uploading your file." });
-			throw error;
-		});
-		const iconFile: any | undefined = req.files["icon"][0];
-		const modelFile: any | undefined = req.files["model"][0];
 
 		if (!iconFile || !modelFile) {
 			res.status(400).json({ error: "Invalid input" });
@@ -98,10 +123,11 @@ interface Item {
 		let item: Item = {
 			name: name,
 			id: generateUUID(),
-			modelPath: `/models/${modelFileName}/`,
-			iconPath: `/icons/${iconFileName}/`,
+			modelPath: `/models/${modelFileName}`,
+			iconPath: `/icons/${iconFileName}`,
 		}
-		
+
+
 		await collection.insertOne(item);
 		res.status(200).json({ item });
 	});
